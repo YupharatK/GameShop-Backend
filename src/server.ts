5cc7bdaa181388e14014852ee0ee258cad91666b
YupharatK <yupharatkhuakkiao04@gmail.com>
2025-10-12 02:32:28 +0700

    Checkout

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import gameRoutes from './routes/game.routes.js';
 import rankingRoutes from './routes/ranking.routes.js';
 import gameTypeRoutes from './routes/gameType.routes.js';
 import walletRoutes from './routes/wallet.routes.js';
+import orderRoutes from './routes/order.routes.js';
 // import userRoutes from './routes/user.routes'; // ถ้ามี
 
 const app = express();
@@ -26,6 +27,7 @@ app.use('/api/games', gameRoutes);
 app.use('/api/rankings', rankingRoutes);
 app.use('/api/gametypes', gameTypeRoutes);
 app.use('/api/wallet', walletRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the GameShop API!</h1><p>Server is running correctly.</p>');
